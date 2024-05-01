08b7b05569b1f081ae04a4a688ff1a8c06f24a64
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-01 11:51:27 -0600

    mission statement pt2

--- javascript/mission.js
+++ javascript/mission.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded',  function() {
-    const selectElement = this.documentElement.getElementById('theme');
-    selectElement.addEventListener
-}
+
+
+
+document.addEventListener('DOMContentLoaded',  
+    function () {
+        const selectElement = document.getElementById("theme");
+        selectElement.addEventListener('change', function() {
+            const selectedValue = this.value;
+            if (selectedValue === 'dark') {
+            document.body.classList.add('dark');
+            } else {
+                document.body.classList.remove('dark');
+            }
+        })
+    }
+);
