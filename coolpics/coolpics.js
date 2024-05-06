6296a00af9cac9040c7dee31a083d96c6f40ad7a
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-06 11:06:57 -0600

    week3

---
+++ coolpics/coolpics.js
@@ -0,0 +1,5 @@
+let image = document.getElementById('image');
+let newImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4k5ZfrL-phOgfTayWaCMQ7ovjK4RroS5YLAxtg0de9Q&s";
+menuButton.addEventListener('click', function () {
+    newImageUrl = imageInput.value;
+});
