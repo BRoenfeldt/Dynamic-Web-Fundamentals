b3b9f7f03b48814fb2334442a62e9fee330c0b07
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-13 11:09:00 -0600

    week4

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,5 +1,9 @@
-let image = document.getElementById('image');
-let newImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4k5ZfrL-phOgfTayWaCMQ7ovjK4RroS5YLAxtg0de9Q&s";
-menuButton.addEventListener('click', function () {
-    newImageUrl = imageInput.value;
-});
+
+document.addEventListener('DOMContentLoaded',  
+    function () {
+        const selectElement = document.getElementsByClassName("menuButton");
+        selectElement.addEventListener('click', function() {
+            console.log("clicked");
+        })
+    }
+);
