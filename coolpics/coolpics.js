7942536de6ac78550a16ebbfe3853d5bef6a6b87
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-14 20:04:53 -0600

    week4

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -4,7 +4,18 @@ function buttonClickPrint(){
     menu.classList.toggle('hidden')  
 }
 
+function viewHandler(){
+    const view = document.getElementById("viewCloser");
+    view.classList.toggle('hidden')
+}
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+      <button class="close-viewer">X</button>
+      <img src="${pic}" alt="${alt}">
+      </div>`;
+  }
+
+
 const button = document.getElementById("menuButton");
 button.addEventListener('click', buttonClickPrint);
-
-const screenSize = innerWidth
