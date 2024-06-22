5178b944d2d13986cdcb195026ebe4f0147d7479
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-06-22 10:18:04 -0600

    week9

--- recipebook/recipe.mjs
+++ recipebook/recipe.mjs
@@ -7,7 +7,7 @@ const recipes = [
 		datePublished: '2016-10-16',
 		tags: ['Waffles', 'Sweet Potato', 'Side'],
 		description: 'Savory waffles made with Sweet potato with a hint of Ginger',
-		image: './images/sweet-potato-waffle.jpg',
+		image: './images/sweet-potato-waffle-md.jpg',
 		recipeIngredient: [
 			'2 separated eggs',
 			'1/4 C Oil',
@@ -280,4 +280,45 @@ const recipes = [
 	}
 ]
 
-export default recipes
+
+// Start code for adding recipes
+function renderRecipes() {
+    const recipePlacer = document.getElementById('recipePlacer');
+  
+    recipes.forEach(recipes => {
+      const recipeInsert = `
+		<div id="recipes">
+			<p id="author">${recipes.author}</p>
+			<a href="${recipes.url}"id="url">${recipes.url}</a>
+			<p id="isBasedOn">${recipes.isBasedOn}</p>
+			<p id="cookTime">${recipes.cookTime}</p>
+			<p id="datePublished">${recipes.datePublished}</p>
+			<p id="tags">${recipes.tags}</p>
+			<p id="description">${recipes.description}</p>
+			<img id="image" src="${recipes.image}" alt="recipe image">
+			<p id="recipeIngredient">${recipes.recipeIngredient}</p>
+			<p id="name">${recipes.name}</p>
+			<p id="prepTime">${recipes.prepTime}</p>
+			<p id="recipeInstructions">${recipes.recipeInstructions}</p>
+			<p id="recipeYield">${recipes.recipeYield}</p>
+			<span class="rating" role="img" aria-label="Rating: ${recipes.rating} out of 5 stars">
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star">⭐</span>
+				<span aria-hidden="true" class="icon-star-empty">⭐</span>
+				<span aria-hidden="true" class="icon-star-empty">☆</span>
+			</span>
+		</div>
+		<br>		
+      `;
+  
+      recipePlacer.insertAdjacentHTML('beforeend', recipeInsert);
+    });
+  }
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderRecipes();
+});
+  // End Code for adding multiple recipes
+
+  //export default recipes
